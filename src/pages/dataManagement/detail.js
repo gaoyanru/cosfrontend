51df707eb289a0f3cf4334bbb4800d30eec347f8
gaoyanru <gaoyr@i-counting.cn>
2018-05-04 16:57:31 +0800

    [fix] 增加详情页返回按钮 related to issue #5

--- src/pages/dataManagement/detail.js
+++ src/pages/dataManagement/detail.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import styles from '@/stylus/modifydata'
-import { Card } from 'antd'
+import { Card, Button } from 'antd'
 import AgentData from '@/containers/dataManagement/Agentdata'
 class ModifyData extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      CompanyName: '北京爱康定'
+    }
+    this.back = this.back.bind(this)
+  }
   onSearch (res) {
     console.log(res)
     let params = {
@@ -11,12 +18,23 @@ class ModifyData extends React.Component {
       connector: res[2]
     }
   }
+  back () {
+    this.props.history.go(-1)
+  }
   render () {
     return (
-      <div style={{ margin: '24px 24px 0' }}>
-        <Card title='Agent数据'>
-          <AgentData/>
-        </Card>
+      <div>
+        <div style={{position: 'relative'}}>
+          <h4 className={styles.title}>{this.state.CompanyName}</h4>
+          <Button className={styles.btn} type="primary" onClick={this.back}>
+            返回
+          </Button>
+        </div>
+        <div style={{ margin: '24px 24px 0' }}>
+          <Card title='Agent数据'>
+            <AgentData/>
+          </Card>
+        </div>
       </div>
     )
   }
