93120da59793c24bb5df4e7d1bdb9b8c2e9dd32b
gaoyanru <gaoyr@i-counting.cn>
2018-05-04 17:44:21 +0800

    [fix] tab related to issue #5

--- src/pages/dataManagement/detail.js
+++ src/pages/dataManagement/detail.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from '@/stylus/modifydata'
-import { Card, Button } from 'antd'
+import { Card, Button, Tabs } from 'antd'
 import AgentData from '@/containers/dataManagement/Agentdata'
+const TabPane = Tabs.TabPane
 class ModifyData extends React.Component {
   constructor (props) {
     super(props)
@@ -9,6 +10,7 @@ class ModifyData extends React.Component {
       CompanyName: '北京爱康定'
     }
     this.back = this.back.bind(this)
+    this.callback = this.callback.bind(this)
   }
   onSearch (res) {
     console.log(res)
@@ -20,6 +22,9 @@ class ModifyData extends React.Component {
   }
   back () {
     this.props.history.go(-1)
+  }
+  callback (key) {
+    console.log(key)
   }
   render () {
     return (
@@ -34,6 +39,22 @@ class ModifyData extends React.Component {
           <Card title='Agent数据'>
             <AgentData/>
           </Card>
+        </div>
+        <div className={styles.con} style={{ margin: '24px 24px 0' }}>
+          <Tabs defaultActiveKey="1" onChange={this.callback}>
+            <TabPane className={styles['basic-info']} tab="订单" key="1">
+              <div>ssss</div>
+            </TabPane>
+            <TabPane className={styles['basic-info']} tab="外勤任务信息" key="2">
+              <div>sssss</div>
+            </TabPane>
+            <TabPane className={styles['basic-info']} tab="记账服务" key="3">
+              <div>sssss</div>
+            </TabPane>
+            <TabPane className={styles['basic-info']} tab="操作记录" key="4">
+              <div>sssss</div>
+            </TabPane>
+          </Tabs>
         </div>
       </div>
     )
